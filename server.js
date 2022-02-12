a35257ba129d1acc19bbf5d5c4f57098b1c5c55e
kenannn <kenanjoshua3454@gmail.com>
2022-02-12 15:23:02 +0800

    backend(node): query calls

--- server.js
+++ server.js
@@ -1,9 +1,16 @@
 const http = require('http');
-const networkInterface = require('os');
+const fs = require('fs');
+const url = require('url');
+
+const data = fs.readFileSync('templates/index.html', 'utf-8');
+const style = fs.readFileSync('templates/styles.css', 'utf-8');
 
 const server = http.createServer((request, response) => {
-	console.log(request.socket.localAddress);
-	response.end('Hello World');
+	const query = url.parse(request.url, true).query.data;
+	if (query) {
+		console.log(query);
+	}
+	response.end(data.replace('{{style}}', style));
 });
 
 server.listen('8000', '127.0.0.1');
