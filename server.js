d2ebed70975c7a99d2512f4e9edf22566db4216c
kenannn <kenanjoshua3454@gmail.com>
2022-02-12 15:48:01 +0800

    backend(node): component lifecycle

--- server.js
+++ server.js
@@ -2,15 +2,24 @@ const http = require('http');
 const fs = require('fs');
 const url = require('url');
 
-const data = fs.readFileSync('templates/index.html', 'utf-8');
+const template = fs.readFileSync('templates/index.html', 'utf-8');
 const style = fs.readFileSync('templates/styles.css', 'utf-8');
+const data = fs.readFileSync('data.json', 'utf-8');
+const dataObj = JSON.parse(data);
+let output = template.replace('{{style}}', style);
+output = template.replace('{{data}}', dataObj.name);
 
 const server = http.createServer((request, response) => {
 	const query = url.parse(request.url, true).query.data;
 	if (query) {
-		console.log(query);
+		// output = output.replace('{{data}}', query);
+		dataObj.name = query;
+		fs.writeFileSync('data.json', JSON.stringify(dataObj));
+		output = template.replace('{{data}}', dataObj.name);
+		response.end(output);
+	} else {
+		response.end(output);
 	}
-	response.end(data.replace('{{style}}', style));
 });
 
 server.listen('8000', '127.0.0.1');
